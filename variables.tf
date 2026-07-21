8a5dca99cf240f106e68fb1c177a3381476957f1
dr-musa-bala <bilalymedicals@gmail.com>
2026-07-21 13:05:20 +0300

    feat: test feature branch pull request workflow

--- variables.tf
+++ variables.tf
@@ -9,3 +9,9 @@ variable "project_name" {
   description = "The name of the core application stack"
   default     = "sillypets"
 }
+
+variable "image_tag" {
+  type        = string
+  description = "The specific GitHub SHA tag of the container image to deploy"
+  default     = "latest"
+}
